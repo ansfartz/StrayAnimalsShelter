f6e758d9b41236ca54732b4f87b2eca7fbdc7be5
XKoutax <carolsfartz@gmail.com>
2019-02-19 21:17:46 +0200

    Added Firebase and login_Activity

---
+++ app/src/main/java/com/bcs/andy/strayanimalsshelter/MyFirebaseApp.java
@@ -0,0 +1,17 @@
+package com.bcs.andy.strayanimalsshelter;
+
+import android.app.Application;
+
+import com.firebase.client.Firebase;
+
+
+
+public class MyFirebaseApp extends Application {
+
+    public void onCreate() {
+        super.onCreate();
+
+        Firebase.setAndroidContext(this);
+    }
+
+}
